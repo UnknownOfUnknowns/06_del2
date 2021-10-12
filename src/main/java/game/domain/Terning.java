619eb5bcaa3b1566876565906f547c1797dac328
Unknown Unknown <hans@bilstrupandersen.dk>
2021-10-12 15:33:22 +0200

    Terning implementeret og testet

---
+++ src/main/java/game/domain/Terning.java
@@ -0,0 +1,17 @@
+package game.domain;
+
+public class Terning {
+    private int værdi;
+    private final int SIDER;
+    public Terning(int sider){
+        SIDER = sider;
+        værdi = 1;
+    }
+    public void rul(){
+        værdi = (int) (Math.random()*SIDER)+1;
+    }
+
+    public int getVærdi() {
+        return værdi;
+    }
+}
