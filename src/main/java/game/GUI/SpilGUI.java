a648d880e19fc6a36ffde82a4719ac1f81e98afb
Unknown Unknown <hans@bilstrupandersen.dk>
2021-10-22 15:06:14 +0200

    Load af felter ind i GUI opdateret

--- src/main/java/game/GUI/SpilGUI.java
+++ src/main/java/game/GUI/SpilGUI.java
@@ -1,10 +1,9 @@
 package game.GUI;
 
+import com.sun.source.tree.UsesTree;
 import game.Util.FeltConfigLoader;
 import gui_codebehind.GUI_FieldFactory;
-import gui_fields.GUI_Empty;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Street;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -12,17 +11,33 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class SpilGUI {
-
+    private static GUI_Player p1;
+    private static GUI_Player p2;
+    private static GUI gui;
     public static void main(String[] args) throws FileNotFoundException {
-        GUI gui = new GUI(loadFelter());
+        setUp();
+        spil();
     }
 
+    private static void spil(){
+
+    }
+
+    private static void setUp() throws FileNotFoundException {
+        p1 = new GUI_Player("Spiller 1", 1000);
+        p2 = new GUI_Player("Spiller 2", 1000);
+        gui = new GUI(loadFelter());
+        gui.addPlayer(p1);
+        gui.addPlayer(p2);
+        gui.setDice(1,1);
+    }
 
     private static GUI_Field[] loadFelter() throws FileNotFoundException {
         Scanner input = FeltConfigLoader.getInstance().getFeltInput();
-        ArrayList<GUI_Field> felter = new ArrayList<GUI_Field>();
+        ArrayList<GUI_Field> felter = new ArrayList<>();
         while(input.hasNext()){
             felter.add(new GUI_Street(input.next(), "Pris: " + input.next(),"", "", Color.blue, Color.black));
         }
