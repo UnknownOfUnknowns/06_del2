91d5f73f4678f5f75cbd7668e74f5e40a500fc6f
Hurtigpilskade <albertmatthaihalkjaer@gmail.com>
2021-10-27 12:57:44 +0200

    Spil klasse / tur_tagning done

--- src/main/java/game/domain/Spil.java
+++ src/main/java/game/domain/Spil.java
@@ -1,19 +1,42 @@
 package game.domain;
 
+import java.io.FileNotFoundException;
+
 public class Spil {
     private Spiller s1;
     private Spiller s2;
     private Spiller vinder;
     private Spiller tur_spiller;
+    private Bræt spillebræt;
 
-    public Spil(Spiller s1, Spiller s2){
+    public Spil(Spiller s1, Spiller s2) throws FileNotFoundException {
         this.s1 = s1;
         this.s2 = s2;
         vinder = null;
         tur_spiller = s1;
+        spillebræt = new Bræt();
     }
     private boolean har_vinder(){
+        vinder = s1.getKonto().getSaldo() >= 4000 ? s1 : null;
+        vinder = s2.getKonto().getSaldo() >= 4000 ? s2 : null;
         return vinder != null;
+    }
+    public void tag_tur(Spiller s) throws Exception {
+        if (s != tur_spiller)
+            throw new Exception("spiller ud af tur");
+        int øjne = s.rul_terninger();
+        int virkning = spillebræt.getVirkning(øjne);
+        s.getKonto().påvirkBalance(virkning);
+        Felt felt = spillebræt.getFelt(øjne);
+        s.setFelt(felt);
+        if (har_vinder())
+            return;
+        if (felt.giverEkstraTur()){
+            tag_tur(s);
+        }
+        else {
+            tur_spiller = s == s1 ? s2 : s1;
+        }
     }
 
 }
--- src/main/java/game/domain/Spiller.java
+++ src/main/java/game/domain/Spiller.java
@@ -5,11 +5,13 @@ public class Spiller {
     private Terning t1;
     private Terning t2;
     private static final int TERNING_SIDER = 6;
+    private Felt felt;
 
-    public Spiller(){
+    public Spiller(Felt f){
         konto = new Konto(1000);
         t1 = new Terning(TERNING_SIDER);
         t2 = new Terning(TERNING_SIDER);
+        felt = f;
     }
 
     public int rul_terninger(){
@@ -30,6 +32,14 @@ public class Spiller {
     public int[] getStatus(){
         int[] øjne = getTerningØjne();
         return new int[]{øjne[0], øjne[1], konto.getSaldo()};
+    }
+
+    public Felt getFelt() {
+        return felt;
+    }
+
+    public void setFelt(Felt felt) {
+        this.felt = felt;
     }
 }
 
